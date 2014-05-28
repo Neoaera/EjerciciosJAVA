5a29003a56746a53561034d2e3e0ef30b368dbad
Neoaera <aera_10@hotmail.com>
2014-05-28 19:03:19 -0400

    Valida Credenciales, version no-funcional eso todavia no :33

---
+++ src/com/javarevolutions/ws/bean/beanLogin.java
@@ -0,0 +1,34 @@
+package com.javarevolutions.ws.bean;
+
+import java.io.Serializable;
+
+public class beanLogin implements Serializable{
+private String usuario;
+private String password;
+private String mensaje;
+private Boolean status;
+public String getUsuario() {
+	return usuario;
+}
+public void setUsuario(String usuario) {
+	this.usuario = usuario;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getMensaje() {
+	return mensaje;
+}
+public void setMensaje(String mensaje) {
+	this.mensaje = mensaje;
+}
+public Boolean getStatus() {
+	return status;
+}
+public void setStatus(Boolean status) {
+	this.status = status;
+}
+}
